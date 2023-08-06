9ba6d6d98995aefcc8f93b6d0d23623f1f57d763
Isaac Johnson <droonspy@gmail.com>
2023-08-06 16:13:13 +0000

    further testing

--- App.js
+++ App.js
@@ -3,14 +3,18 @@ import { useState, useEffect } from 'react';
 import { StyleSheet, Text, TextComponent, View } from 'react-native';
 
 export default function App() {
+  const [URL, setURL] = useState("https://abs.isaac.one")
+
   const [status, setStatus] = useState("")
   const [username, setUsername] = useState("")
   const [password, setPassword] = useState("")
   const [userID, setUserID] = useState("")
   const [userToken, setUserToken] = useState("")
+  const [libraries, setLibraries] = useState([])
+  const [libraryItems, setLibraryItems] = useState([])
 
   async function getStatus() {
-    return fetch('https://abs.isaac.one/status')
+    return fetch(`${URL}/status`)
       .then(response => response.json())
       .then(json => `Initialized: ${json.isInit}\nLanguage: ${json.language}`)
       .then(string => setStatus(string));
@@ -19,9 +23,7 @@ export default function App() {
   async function getLogin() {
     let body = {"username": username, "password": password}
 
-    console.log(body)
-
-    let response = await fetch('https://abs.isaac.one/login', {
+    let response = await fetch(`${URL}/login`, {
       method: 'POST', 
       body: JSON.stringify(body),
       headers: {
@@ -33,25 +35,85 @@ export default function App() {
 
     setUserID(response.user.id)
     setUserToken(response.user.token)
+
+    getLibraries(response.user.token)
+  }
+
+  async function getLibraries(token) {
+    let newLibraries = []
+
+    let response = await fetch(`${URL}/api/libraries`, {
+      method: 'GET',
+      headers: {
+        "Authorization": `Bearer ${token}`
+      }
+    })
+
+    response = await response.json()
+
+    response.libraries.forEach(library => {
+      newLibraries.push(library.name)
+    });
+
+    setLibraries(newLibraries.join('\r\n'))
+    getLibraryItems(token, response.libraries[0].id)
+  }
+
+  async function getLibraryItems(token, libraryID) {
+    let items = []
+
+    let response = await fetch(`${URL}/api/libraries/${libraryID}/items`, {
+      method: 'GET',
+      headers: {
+        "Authorization": `Bearer ${token}`
+      }
+    }).then(response => response.json())
+      .then(json => { return json })
+
+    response.results.forEach(result => {
+      items.push(result.media.metadata.title)
+    })
+
+    setLibraryItems(items.join('\r\n'))
+
   }
 
   return (
-    <View style={styles.container}>
-      <h1>Server</h1>
-      <label htmlFor='ip'>Server URL:</label>
-      <input type='text' id='ip' name='ip' defaultValue={'https://abs.isaac.one'}></input>
-      <button onClick={getStatus}>Check Status</button>
-      <Text>{status}</Text>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen
+          name="Home"
+          component={HomeScreen}
+          options={{title: 'Welcome'}}
+        />
+        <Stack.Screen 
+          name="Profile" 
+          component={ProfileScreen} 
+        />
+      </Stack.Navigator>
+      <View style={styles.container}>
+        <h1 style={styles.header}>Server</h1>
+        <label htmlFor='ip'>Server URL:</label>
+        <input type='text' id='ip' name='ip' value={URL} onChange={event => setURL(event.target.value)}></input>
+        <button onClick={getStatus}>Check Status</button>
+        <Text>{status}</Text>
 
-      <h1>Login</h1>
-      <label htmlFor="username">Username:</label>
-      <input type="text" id="username" name="username" value={username} onChange={event => setUsername(event.target.value)}></input>
-      <label htmlFor="pwd">Password:</label>
-      <input type="password" id="pwd" name="pwd" value={password} onChange={event => setPassword(event.target.value)}></input>
-      <button onClick={getLogin}>Login</button>
-      <Text>ID: {userID}</Text>
-      <Text>Token: {userToken}</Text>
-    </View>
+        <h1 style={styles.header}>Login</h1>
+        <label htmlFor="username">Username:</label>
+        <input type="text" id="username" name="username" value={username} onChange={event => setUsername(event.target.value)}></input>
+        <label htmlFor="pwd">Password:</label>
+        <input type="password" id="pwd" name="pwd" value={password} onChange={event => setPassword(event.target.value)}></input>
+        <button onClick={getLogin}>Login</button>
+        <Text>ID: {userID}</Text>
+        <Text>Token: {userToken}</Text>
+
+        <h1 style={styles.header}>Libraries</h1>
+        <Text>{libraries}</Text>
+
+        <h1 style={styles.header}>Items</h1>
+        <Text>{libraryItems}</Text>
+      </View>
+    </NavigationContainer>
   );
 }
 
@@ -59,7 +121,12 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: 'start',
+    justifyContent: 'top',
+    fontFamily: 'sans-serif'
   },
+  header: {
+    margin: 0,
+    padding: 0
+  }
 });
